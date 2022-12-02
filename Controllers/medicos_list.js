d8ce4d4584105a773b05970c00ff147a29de05a5
Martin Antonio <mancorge@gmail.com>
2022-12-02 19:48:57 +0100

    Algunas actualizaciones en el listado de medicos, añadidos botones de eliminar y editar

--- Controllers/medicos_list.js
+++ Controllers/medicos_list.js
@@ -2,13 +2,18 @@ $(document).ready(function(){
     $("#search_button").click(function(){
         $.ajax({
             method: "POST",
-            url: "web_services/medicos_list.php",
-            data:{},
+            url: "./web_services/medicos_list.php",
+            data:{
+                nombre: $("#nombre").val(),
+                apellido1: $("#apellido1").val(),
+                numero_colegiado: $("#numero_colegiado").val(),
+            },
             dataType: "json",
         })
         .done(function(response){
+            // console.log(response)
+            printTableStruct()
             var i = 1
-            printHeaders()
             while(i <= Object.keys(response).length){
                 printResponse(i,response)
                 i++
@@ -17,12 +22,34 @@ $(document).ready(function(){
         .fail(function(jqXHR,textStatus,error){
             alert("Error en conexión ajax")
         })
-
     })
+
+    $("#reset_button").click(function(){
+        $("#tableToPrint").html("")
+    })
+
 })
 
-function printHeaders(){
-    let cabecera = `<table id="myTable" class="list-table">
+
+function printResponse(index,response){
+    // console.log(response[index])
+    let fila =  `<tr>
+                    <td>` + response[index]["numero_colegiado"] + `</td>
+                    <td>` + response[index]["dni"] + `</td>
+                    <td>` + response[index]["nombre"] + `</td>
+                    <td>` + response[index]["apellido1"] + `</td>
+                    <td>` + response[index]["apellido2"] + `</td>
+                    <td>` + response[index]["telefono"] + `</td>
+                    <td>` + response[index]["especialidad"] + `</td>
+                    <td>` + response[index]["horario"] + `</td>
+                    <td><button id="delete_button`+index+`" class="std_button delete_btn">-</button></td>
+                    <td><button id="edit_button`+index+`" class="std_button delete_btn">Editar</button></td>
+                </tr>`
+    $("#myTableData").append(fila)
+}
+
+function printTableStruct(){
+    let estructura = `<table id="myTable" class="list-table">
                     <thead>
                         <tr>
                             <th>Nº colegiado</th>
@@ -33,26 +60,12 @@ function printHeaders(){
                             <th>Teléfono</th>
                             <th>ID Especialidad</th>
                             <th>ID horario</th>
+                            <th colspan="2">Opciones</th>
                          </tr>
                     </thead>
                     <tbody id="myTableData">
                         <!-- Aquí se pintarán los resultados de la consulta -->
                     </tbody>
                     </table>`
-    $("#article1").html(cabecera)
-}
-
-function printResponse(index,response){
-    console.log(response[index])
-    let fila =  `<tr>
-                    <td>` + response[index]["numero_colegiado"] + `</td>
-                    <td>` + response[index]["dni"] + `</td>
-                    <td>` + response[index]["nombre"] + `</td>
-                    <td>` + response[index]["apellido1"] + `</td>
-                    <td>` + response[index]["apellido2"] + `</td>
-                    <td>` + response[index]["telefono"] + `</td>
-                    <td>` + response[index]["especialidad"] + `</td>
-                    <td>` + response[index]["horario"] + `</td>
-                </tr>`
-    $("#myTableData").append(fila)
+    $("#tableToPrint").html(estructura)
 }
